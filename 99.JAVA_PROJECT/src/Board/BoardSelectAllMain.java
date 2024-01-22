f0dbf8e42fb90331a41b921c9aef90eff4993da6
장유진 <yj999900@gmail.com>
2024-01-22 18:09:55 +0900

    .

---
+++ 99.JAVA_PROJECT/src/Board/BoardSelectAllMain.java
@@ -0,0 +1,10 @@
+package Board;
+
+public class BoardSelectAllMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 99.JAVA_PROJECT/src/Board/BoardSelectByPrimaryKeyMain.java
@@ -0,0 +1,10 @@
+package Board;
+
+public class BoardSelectByPrimaryKeyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
