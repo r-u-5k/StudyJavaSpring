9d642d6c423f7d8b7e20c72d18b3d120bf8a7474
장유진 <yj999900@gmail.com>
2024-01-26 09:35:09 +0900

    .

--- 97.JAVA_APPLICATION_ARCHITECTURE_YJ/src/main/java/com/itwill/member/MemberDao.java
+++ 97.JAVA_APPLICATION_ARCHITECTURE_YJ/src/main/java/com/itwill/member/MemberDao.java
@@ -19,26 +19,27 @@ public class MemberDao {
 	 * Dao 객체는멤버변수로 Connection을 생성하는 DataSource객체를 가짐
 	 */
 	private DataSource dataSource;
-	public MemberDao()  throws Exception{
-		dataSource=new DataSource();
+
+	public MemberDao() throws Exception {
+		dataSource = new DataSource();
 	}
-	
-	
-	public int insert(Member newMember) throws Exception{
+
+	public int insert(Member newMember) throws Exception {
 		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(MemberSQL.MEMBER_INSERT);
-		pstmt.setString(1,newMember.getMemberId());
-		pstmt.setString(2,newMember.getMemberPassword());
-		pstmt.setString(3,newMember.getMemberName());
-		pstmt.setString(4,newMember.getMemberAddress());
+		PreparedStatement pstmt = con.prepareStatement(MemberSQL.MEMBER_INSERT);
+		pstmt.setString(1, newMember.getMemberId());
+		pstmt.setString(2, newMember.getMemberPassword());
+		pstmt.setString(3, newMember.getMemberName());
+		pstmt.setString(4, newMember.getMemberAddress());
 		pstmt.setInt(5, newMember.getMemberAge());
 		pstmt.setString(6, newMember.getMemberMarried());
-		int insertRowCount=pstmt.executeUpdate();
+		int insertRowCount = pstmt.executeUpdate();
 		return insertRowCount;
 	}
-	public int update(Member updateMember)throws Exception {
+
+	public int update(Member updateMember) throws Exception {
 		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(MemberSQL.MEMBER_UPDATE);
+		PreparedStatement pstmt = con.prepareStatement(MemberSQL.MEMBER_UPDATE);
 		pstmt.setString(1, updateMember.getMemberPassword());
 		pstmt.setString(2, updateMember.getMemberName());
 		pstmt.setString(3, updateMember.getMemberAddress());
@@ -49,65 +50,40 @@ public class MemberDao {
 		int updateRowCount = pstmt.executeUpdate();
 		return updateRowCount;
 	}
-	public int delete(String member_id) throws Exception{
+
+	public int delete(String member_id) throws Exception {
 		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(MemberSQL.MEMBER_DELETE);
+		PreparedStatement pstmt = con.prepareStatement(MemberSQL.MEMBER_DELETE);
 		pstmt.setString(1, member_id);
-		int deleteRowCount=pstmt.executeUpdate();
+		int deleteRowCount = pstmt.executeUpdate();
 		return deleteRowCount;
 	}
-	public Member findByMemberId(String member_id)throws Exception {
+
+	public Member findByMemberId(String member_id) throws Exception {
 		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(MemberSQL.MEMBER_SELECT_BY_ID);
-		pstmt.setString(1,member_id);
-		ResultSet rs=pstmt.executeQuery();
-		
-		Member findMember=null;
-		if(rs.next()) {
-			findMember=new Member( 
-						rs.getString("member_id"),
-						rs.getString("member_password"),
-						rs.getString("member_name"),
-						rs.getString("member_address"),
-						rs.getInt("member_age"),
-						rs.getString("member_married"),
-						rs.getDate("member_regdate"));
+		PreparedStatement pstmt = con.prepareStatement(MemberSQL.MEMBER_SELECT_BY_ID);
+		pstmt.setString(1, member_id);
+		ResultSet rs = pstmt.executeQuery();
+
+		Member findMember = null;
+		if (rs.next()) {
+			findMember = new Member(rs.getString("member_id"), rs.getString("member_password"),
+					rs.getString("member_name"), rs.getString("member_address"), rs.getInt("member_age"),
+					rs.getString("member_married"), rs.getDate("member_regdate"));
 		}
 		return findMember;
 	}
-	public ArrayList<Member> findAll() throws Exception{
+
+	public ArrayList<Member> findAll() throws Exception {
 		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(MemberSQL.MEMBER_SELECT_ALL);
-		ResultSet rs=pstmt.executeQuery();
-		ArrayList<Member> memberList=new ArrayList<Member>();
-		while(rs.next()) {
-			memberList.add(new Member(
-						rs.getString("member_id"),
-						rs.getString("member_password"),
-						rs.getString("member_name"),
-						rs.getString("member_address"),
-						rs.getInt("member_age"),
-						rs.getString("member_married"),
-						rs.getDate("member_regdate"))
-					 );
+		PreparedStatement pstmt = con.prepareStatement(MemberSQL.MEMBER_SELECT_ALL);
+		ResultSet rs = pstmt.executeQuery();
+		ArrayList<Member> memberList = new ArrayList<Member>();
+		while (rs.next()) {
+			memberList.add(new Member(rs.getString("member_id"), rs.getString("member_password"),
+					rs.getString("member_name"), rs.getString("member_address"), rs.getInt("member_age"),
+					rs.getString("member_married"), rs.getDate("member_regdate")));
 		}
 		return memberList;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- 97.JAVA_APPLICATION_ARCHITECTURE_YJ/src/main/java/com/itwill/member/MemberService.java
+++ 97.JAVA_APPLICATION_ARCHITECTURE_YJ/src/main/java/com/itwill/member/MemberService.java
@@ -4,56 +4,60 @@ import java.util.List;
 
 public class MemberService {
 	private MemberDao memberDao;
-	public MemberService() throws Exception{
-		memberDao=new MemberDao();
+
+	public MemberService() throws Exception {
+		memberDao = new MemberDao();
 	}
+
 	/*
 	 * 회원가입
 	 */
-	public boolean addMember(Member newMember) throws Exception{
-		boolean isSuccess=false;
+	public boolean addMember(Member newMember) throws Exception {
+		boolean isSuccess = false;
 		/*
 		 * 아이디존재여부
 		 */
-	    
-	    return isSuccess;
+
+		return isSuccess;
 	}
-	
+
 	/*
 	 * 회원로그인
 	 */
-	public Member login(String memberId,String memberPassword)throws Exception{
+	public Member login(String memberId, String memberPassword) throws Exception {
 		return null;
 	}
-	
+
 	/*
 	 * 회원아이디중복체크
 	 */
-	
-	
+
 	/*
 	 * 회원상세보기
 	 */
-	public Member memberDetail(String memberId) throws Exception{
+	public Member memberDetail(String memberId) throws Exception {
 		return null;
 	}
+
 	/*
 	 * 회원수정
 	 */
-	public int memberUpdate(Member member)throws Exception{
+	public int memberUpdate(Member member) throws Exception {
 		return 0;
 	}
+
 	/*
 	 * 회원탈퇴
 	 */
-	public int memberDelete(String memberId) throws Exception{
+	public int memberDelete(String memberId) throws Exception {
 		return 0;
 	}
-	/*************************admin***************************/
+
+	/************************* admin ***************************/
 	/*
 	 * 회원전체리스트
 	 */
-	public List<Member> memberList()throws Exception{
+	public List<Member> memberList() throws Exception {
 		return null;
 	}
 	/* 
