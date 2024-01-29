d15813960856c573d88c1434bde153e482eae510
장유진 <yj999900@gmail.com>
2024-01-29 15:32:17 +0900

    .

---
+++ JAVA_PROJECT_SHOP/산출물/02.테이블정의서/shop_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO userinfo (userId, password, name, email) VALUES ('yj9900', '1234', '장유진', 'yj999900@gmail.com');
+INSERT INTO userinfo (userId, password, name, email) VALUES ('yj1111', '1111', 'INSERT1', 'insert1@gmail.com');
+INSERT INTO userinfo (userId, password, name, email) VALUES ('yj2222', '2222', 'INSERT2', 'insert2@gmail.com');
+INSERT INTO userinfo (userId, password, name, email) VALUES ('yj3333', '3333', 'INSERT3', 'insert3@gmail.com');
