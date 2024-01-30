bc71039e4b2a060e8cc6c483496ffe21fba4bc72
장유진 <yj999900@gmail.com>
2024-01-30 13:57:37 +0900

    .

--- JAVA_PROJECT_SHOP/산출물/02.테이블정의서/shop_insert.sql
+++ JAVA_PROJECT_SHOP/산출물/02.테이블정의서/shop_insert.sql
@@ -1,4 +1,48 @@
+-- MEMBER INSERT
 INSERT INTO userinfo (userId, password, name, email) VALUES ('yj9900', '1234', '장유진', 'yj999900@gmail.com');
 INSERT INTO userinfo (userId, password, name, email) VALUES ('yj1111', '1111', 'INSERT1', 'insert1@gmail.com');
 INSERT INTO userinfo (userId, password, name, email) VALUES ('yj2222', '2222', 'INSERT2', 'insert2@gmail.com');
 INSERT INTO userinfo (userId, password, name, email) VALUES ('yj3333', '3333', 'INSERT3', 'insert3@gmail.com');
+
+-- PRODUCT INSERT
+INSERT INTO product (p_no, p_name, p_price, p_image, p_desc, p_click_count) VALUES (1, '비글', 55000, 'beagle.png', 'beagle', 0);
+insert into product values(1, '비글', 550000, 'bigle.png','기타 상세 정보 등...', 0);
+insert into product values(2, '달마시안', 500000, 'dalma.jpg','기타 상세 정보 등...', 0);
+insert into product values(3, '퍼그', 400000, 'pug.jpg','기타 상세 정보 등...', 0);
+insert into product values(4, '페키니즈', 450000, 'pekiniz.png','기타 상세 정보 등...', 0);
+insert into product values(5, '포메라니안', 800000, 'pomeranian.jpg','기타 상세 정보 등...', 0);
+insert into product values(6, '샤페이', 700000, 'shaipei.jpg','기타 상세 정보 등...', 0);
+insert into product values(7, '닥스훈트', 800000, 'dachshund.jpg','기타 상세 정보 등...', 0);
+insert into product values(8, '사모예드', 800000, 'samoyed.jpg','기타 상세 정보 등...', 0);
+
+-- CART INSERT
+INSERT INTO cart (cart_no, userId, p_no, cart_qty) VALUES (cart_cart_no_seq.nextval, 'yj9900', 1, 2);
+INSERT INTO cart (cart_no, userId, p_no, cart_qty) VALUES (cart_cart_no_seq.nextval, 'yj9900', 3, 1);
+INSERT INTO cart (cart_no, userId, p_no, cart_qty) VALUES (cart_cart_no_seq.nextval, 'yj9900', 7, 4);
+
+-- ORDER INSERT
+-- CART 안에 있는 아이템들 주문
+-- yj9900 주문
+-- ORDERS
+INSERT INTO ORDERS (O_NO, O_DESC, O_DATE, O_PRICE, USERID) VALUES (orders_o_no_seq.nextval, '비글외2종', SYSDATE, 1750000, 'yj9900');
+-- ORDER_ITEM
+INSERT INTO ORDER_ITEM (OI_NO, OI_QTY, O_NO, P_NO) VALUES (order_item_oi_no_seq.nextval, 2, orders_o_no_seq.currval, 1);
+INSERT INTO ORDER_ITEM (OI_NO, OI_QTY, O_NO, P_NO) VALUES (order_item_oi_no_seq.nextval, 1, orders_o_no_seq.currval, 3);
+INSERT INTO ORDER_ITEM (OI_NO, OI_QTY, O_NO, P_NO) VALUES (order_item_oi_no_seq.nextval, 4, orders_o_no_seq.currval, 7);
+
+-- yj1111 주문 (1번 2개, 3번 1개)
+-- ORDERS
+INSERT INTO ORDERS (O_NO, O_DESC, O_DATE, O_PRICE, USERID) VALUES (orders_o_no_seq.nextval, '비글외1종', SYSDATE, 950000, 'yj1111');
+-- ORDER_ITEM
+INSERT INTO ORDER_ITEM (OI_NO, OI_QTY, O_NO, P_NO) VALUES (order_item_oi_no_seq.nextval, 2, orders_o_no_seq.currval, 1);
+INSERT INTO ORDER_ITEM (OI_NO, OI_QTY, O_NO, P_NO) VALUES (order_item_oi_no_seq.nextval, 1, orders_o_no_seq.currval, 3);
+
+-- ORDER SELECT
+-- 1. 멤버 한 사람의 주문 전체 목록
+SELECT * FROM ORDERS WHERE USERID = 'yj9900';
+-- 2. 멤버 한 사람의 주문 1개
+SELECT * FROM ORDERS WHERE O_NO = 1;
+-- 3. 멤버 한 사람의 주문 1개의 주문 아이템 여러 개, 제품 정보
+SELECT * FROM ORDERS O JOIN ORDER_ITEM OI 
+ON o.o_no = oi.o_no
+WHERE O_NO = 1;
---
+++ JAVA_PROJECT_SHOP/산출물/02.테이블정의서/shop_select_update_delete.sql
@@ -0,0 +1,56 @@
+/***************회원******************/
+--회원정보보기(select pk)
+SELECT * FROM USERINFO WHERE USERID='yj9900';
+
+--회원정보수정(update pk)
+UPDATE USERINFO SET PASSWORD='1111',NAME='김변경',EMAIL='change@naver.com' WHERE USERID='yj9900';
+
+--회원탈퇴(delete pk)
+DELETE USERINFO WHERE USERID='yj9900';
+
+/****************제품*****************/
+--제품전체리스트
+SELECT * FROM PRODUCT;
+--제품상세보기
+SELECT * FROM PRODUCT WHERE P_NO=1;
+--제품수정(X)
+--제품삭제(X)
+--제품추가(X)
+
+/***********************카트************************/
+--로그인한 멤버(guard1)의 카트 리스트
+SELECT * FROM CART WHERE USERID = 'yj9900';
+SELECT C.*, P.*, C.cart_qty * P.p_price "아이템가격" FROM CART C JOIN PRODUCT P
+ON C.P_NO = P.P_NO
+WHERE USERID = 'yj9900';
+
+--로그인한 멤버(guard1)의 카트 리스트 삭제
+DELETE FROM CART WHERE USERID = 'yj9900';
+COMMIT;
+
+--로그인한 멤버(guard1)의 카트 아이템 1개 삭제
+DELETE FROM CART WHERE CART_NO = 9;
+
+--로그인한 멤버(guard1)의 카트 1개 아이템 수량 변경
+UPDATE CART SET cart_qty = cart_qty + 1 WHERE USERID = 'yj9900' AND P_NO = 1;
+
+--로그인한 멤버(guard1)의 카트에 존재하는 제품의 수(제품존재여부판단)
+SELECT COUNT(*) AS P_COUNT FROM CART WHERE USERID = 'yj9900' AND P_NO = 1;
+
+--로그인한 멤버(guard1)의 카트에 담기(존재하는 상품 수정)
+UPDATE CART SET CART_QTY = CART_QTY + 3 WHERE USERID = 'yj9900' AND P_NO = 1;
+
+/****************order********************/
+--1.멤버한사람의 주문전체목록(guard1)
+
+
+--1.멤버한사람의 주문(주문아이템+제품)전체목록(guard1)
+
+
+--2.멤버한사람의 주문(주문아이템+제품)한개(guard1)
+
+
+--3.로그인한멤버(guard1)주문한개삭제
+-- on delete cascade
+
+--4. 로그인한멤버(guard1)주문전체삭제
