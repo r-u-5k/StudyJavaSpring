88ad25fdbbc2dce5fdfcd6fada2d8f77417fd948
장유진 <yj999900@gmail.com>
2024-02-02 16:34:07 +0900

    .

---
+++ 97.JAVA_APPLICATION_ARCHITECTURE_YJ/src/main/java/com/itwill/layout/ProductListFrame.java
@@ -0,0 +1,49 @@
+package com.itwill.layout;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+public class ProductListFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProductListFrame frame = new ProductListFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ProductListFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 417, 643);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		ProductListPanel productListPanel = new ProductListPanel();
+		productListPanel.setPreferredSize(new Dimension(202, 800));
+		contentPane.add(productListPanel, BorderLayout.CENTER);
+	}
+
+}
