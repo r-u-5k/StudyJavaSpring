59a18591b2b5173d87d9ced5223e5aa641cd0f6c
장유진 <yj999900@gmail.com>
2024-02-13 16:38:41 +0900

    .

--- servletSite/src/main/java/com/itwill/servlet/HelloServlet.java
+++ servletSite/src/main/java/com/itwill/servlet/HelloServlet.java
@@ -1,11 +1,12 @@
 package com.itwill.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Servlet implementation class HelloServlet
@@ -13,7 +14,20 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"UTF-8\">");
+		out.println("</head>");
+		out.println("<body>");
+		for (int i = 0; i < 100; i++) {
+			out.println("<h3>Hello Servlet(안녕서블릿)[" + i + "]</h3><hr>");
+		}
+		out.println("</body>");
+		out.println("</html>");
+
 	}
 
 }
