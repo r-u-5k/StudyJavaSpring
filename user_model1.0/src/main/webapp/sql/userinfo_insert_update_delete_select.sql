edeac1d0c051403a8657cdf9a86953673a8a99f6
장유진 <yj999900@gmail.com>
2024-02-23 18:17:08 +0900

    .

--- user_model1.0/src/main/webapp/sql/userinfo_insert_update_delete_select.sql
+++ user_model1.0/src/main/webapp/sql/userinfo_insert_update_delete_select.sql
@@ -1,26 +1,27 @@
+INSERT INTO USERINFO (USERID, PASSWORD, NAME, EMAIL) 
+VALUES ('guard1', '1111', '김경호1', 'guard1@korea.com');
 
-insert into userinfo(userid,password,name,email) values('guard1','1111','김경호1','guard1@korea.com');
-insert into userinfo(userid,password,name,email) values('guard2','2222','김경호2','guard2@korea.com');
-insert into userinfo(userid,password,name,email) values('guard3','3333','김경호3','guard3@korea.com');
+INSERT INTO USERINFO (USERID, PASSWORD, NAME, EMAIL) 
+VALUES ('guard2', '2222', '김경호2', 'guard2@korea.com');
+
+INSERT INTO USERINFO (USERID, PASSWORD, NAME, EMAIL) 
+VALUES ('guard3', '3333', '김경호3', 'guard3@korea.com');
 
 --pk update
-update  userinfo 
-set password='1111',name='이름변경',email='change@naver.com' 
-where userid='guard1';
+UPDATE USERINFO SET PASSWORD = '1111', NAME = '이름변경', EMAIL = 'change@naver.com'
+ WHERE USERID = 'guard1';
 --pk delete
-delete userinfo where userid='guard1';
+DELETE USERINFO WHERE USERID = 'guard1';
 
 --select pk
-select userid,password,name,email from userinfo where userid='guard2';
+SELECT USERID, PASSWORD, NAME, EMAIL FROM USERINFO WHERE USERID = 'guard2';
 
 --select all
-select userid,password,name,email from userinfo;
+SELECT USERID, PASSWORD, NAME, EMAIL FROM USERINFO;
 
 --select count by userid
-select count(*) cnt from userinfo where userid='guard1'; 
-select count(*) cnt from userinfo where userid='guard2'; 
+SELECT COUNT(*) CNT FROM USERINFO WHERE USERID = 'guard1';
 
+SELECT COUNT(*) CNT FROM USERINFO WHERE USERID = 'guard2';
 
-
-
-commit;
+COMMIT;
