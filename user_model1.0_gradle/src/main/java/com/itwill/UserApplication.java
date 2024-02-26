9ae794ecd8d4c2649f9dca73b4fe8ebbaf6c10a0
장유진 <yj999900@gmail.com>
2024-02-26 15:57:32 +0900

    .

--- user_model1.0_gradle/src/main/java/com/itwill/UserApplication.java
+++ user_model1.0_gradle/src/main/java/com/itwill/UserApplication.java
@@ -1,13 +1,15 @@
-package com.itwill.user;
+package com.itwill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-public class Application {
+@ServletComponentScan
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(UserApplication.class, args);
 	}
 
 }
