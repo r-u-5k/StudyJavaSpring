f6bef7d8da00acdcb788de0a9770b55a04357628
장유진 <yj999900@gmail.com>
2024-03-21 12:14:58 +0900

    .

--- spring_application/src/main/java/com/itwill/user/UserServiceImpl.java
+++ spring_application/src/main/java/com/itwill/user/UserServiceImpl.java
@@ -5,31 +5,42 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
 @Service
 public class UserServiceImpl implements UserService {
-	//field injection
+	/*
+	 * Field Injection
+	 */
 	@Autowired
 	private UserDao userDao;
-	
+
 	public UserServiceImpl() {
 		System.out.println("#### UserServiceImpl() : 디폴트생성자호출");
-
 	}
-	//@Autowired 생성자 injection
+	
+	/*
+	 * Constructor Injection
+	 */
+	//@Autowired
 	public UserServiceImpl(UserDao userDao) {
-		System.out.println("#### UserServiceImpl("+userDao+") :생성자호출");
-		this.userDao=userDao;
+		System.out.println("#### UserServiceImpl(" + userDao + ") : 생성자호출");
+		this.userDao = userDao;
 	}
-	//@Autowired setter injection
+	
+	/*
+	 * Setter Injection
+	 */
+	//@Autowired
 	public void setUserDao(UserDao userDao) {
 		System.out.println("#### UserServiceImpl : setUserDao(UserDao userDao) : 메쏘드호출");
 		this.userDao = userDao;
 	}
 
 	public User findUser(String userId) throws Exception {
-		System.out.println("#### UserServiceImpl : findUser("+userId+") 호출");
+		System.out.println("#### UserServiceImpl : findUser(" + userId + ") 호출");
 		return userDao.findUser(userId);
 	}
+
 	public List<User> findUserList() throws Exception {
 		System.out.println("#### UserServiceImpl : findUserList() 호출  ");
 		return userDao.findUserList();
