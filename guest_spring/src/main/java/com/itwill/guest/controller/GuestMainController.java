467a8d7da140917576c5e073d7b8edfaa38ca282
장유진 <yj999900@gmail.com>
2024-03-26 15:59:25 +0900

    .

---
+++ guest_spring/src/main/java/com/itwill/guest/controller/GuestMainController.java
@@ -0,0 +1,19 @@
+package com.itwill.guest.controller;
+
+import com.itwill.spring.mvc.Controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class GuestMainController implements Controller {
+	
+	@Override
+	public String handleRequest(HttpServletRequest request,
+								HttpServletResponse response) {
+		String forwardPath =
+				"forward:/WEB-INF/views/guest_main.jsp";
+		return forwardPath;
+	}
+	
+	
+}
