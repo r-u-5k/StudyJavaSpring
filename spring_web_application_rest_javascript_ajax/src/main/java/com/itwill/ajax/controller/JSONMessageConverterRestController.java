97faa23e065bd401e1d3b1b684fca756937fba07
장유진 <yj999900@gmail.com>
2024-04-04 09:51:14 +0900

    .

--- spring_web_application_rest_javascript_ajax/src/main/java/com/itwill/ajax/controller/JSONMessageConverterRestController.java
+++ spring_web_application_rest_javascript_ajax/src/main/java/com/itwill/ajax/controller/JSONMessageConverterRestController.java
@@ -5,16 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itwill.ajax.domain.News;
-
-import jakarta.websocket.server.PathParam;
 
 @RestController
 public class JSONMessageConverterRestController {
