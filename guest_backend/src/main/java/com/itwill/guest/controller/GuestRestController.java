4ee9e7f999d12008917746deb339799b1d214161
장유진 <yj999900@gmail.com>
2024-04-05 10:48:08 +0900

    .

---
+++ guest_backend/src/main/java/com/itwill/guest/controller/GuestRestController.java
@@ -0,0 +1,14 @@
+package com.itwill.guest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+/*
+ * 1. GET /guests : 방명록 리스트 (모든 데이터 요청)
+ * 2. GET /guests/{guest_no} : 방명록 상세보기 (guest_no 데이터 요청)
+ * 3. POST /guests : 방명록 쓰기 (데이터 추가)
+ * 4. PUT /guests/{guest_no} : 방명록 수정 (guest_no 데이터 수정)
+ * 5. DELETE /guests/{guest_no} : 방명록 삭제 (guest_no 데이터 삭제)
+ */
+@RestController
+public class GuestRestController {
+
+}
