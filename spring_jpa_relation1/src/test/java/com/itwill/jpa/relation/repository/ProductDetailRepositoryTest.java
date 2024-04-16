cdbc0d682a0d990ab2ba453690a1dc1997724aba
Jin <yj999900@gmail.com>
2024-04-16 10:42:06 +0900

    .

--- spring_jpa_relation1/src/test/java/com/itwill/jpa/relation/repository/ProductDetailRepositoryTest.java
+++ spring_jpa_relation1/src/test/java/com/itwill/jpa/relation/repository/ProductDetailRepositoryTest.java
@@ -12,27 +12,36 @@ import com.itwill.jpa.relation.entity.ProductDetail;
 class ProductDetailRepositoryTest extends SpringJpaRelationApplicationTests {
 	@Autowired
 	ProductDetailRepository productDetailRepository;
-	
-//	@Transactional
-//	@Rollback(false)
+
+	@Transactional
+	@Rollback(false)
 	@Test
 	void productDetailWithProductSaveRead() {
 		Product product = Product.builder().name("JPA").price(20000).stock(50).build();
 		ProductDetail productDetail = ProductDetail.builder().description("JPA 책 설명").build();
-									
+
 		/*
 		 연관관계 설정 (OWNER)
 		 ProductDetail --> Product
 		*/
 		productDetail.setProduct(product);
 		productDetailRepository.save(productDetail);
-		
+
 		System.out.println("--------read-----------");
 		ProductDetail findProductDetail = productDetailRepository.findById(1L).get();
 		System.out.println(findProductDetail);
 		Product findProduct = findProductDetail.getProduct();
 		System.out.println(findProduct);
-		
+
 	}
-	
+
+	@Test
+	@Transactional
+	void productDetailWithProductRead() {
+		ProductDetail findProductDetail = productDetailRepository.findById(1L).get();
+		System.out.println("findProductDetail: " + findProductDetail);
+		Product findProduct = findProductDetail.getProduct();
+		System.out.println("findProduct: " + findProduct);
+	}
+
 }
