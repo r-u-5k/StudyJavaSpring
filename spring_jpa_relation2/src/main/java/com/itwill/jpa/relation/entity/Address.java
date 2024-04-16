142d34be7a5f771a0f8c8836bf9e9451b34787b6
Jin <yj999900@gmail.com>
2024-04-16 15:34:21 +0900

    .

--- spring_jpa_relation2/src/main/java/com/itwill/jpa/relation/entity/Address.java
+++ spring_jpa_relation2/src/main/java/com/itwill/jpa/relation/entity/Address.java
@@ -19,38 +19,40 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Entity
 @Table(name = "address")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity
 public class Address {
-	/*
-	  이름 	널? 		유형 
-	  ------- -------- ------------ 
-	  ADDR_ID 	NOT NULL NUMBER(11) 
-	  STREET 	NOT NULL VARCHAR2(50) 
-	  CITY 	NOT NULL VARCHAR2(50) 
-	  STATE 	NOT NULL VARCHAR2(50) 
-	  ZIP				VARCHAR2(10) 
-	  COUNTRY NOT NULL VARCHAR2(50)
-	 */
-	@Id
-	@SequenceGenerator(name = "ADDRESS_ADDR_ID_SEQ", sequenceName = "ADDRESS_ADDR_ID_SEQ", initialValue = 1, allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ADDRESS_ADDR_ID_SEQ")
-	private Long addrId;
-	private String street;
-	private String city;
-	private String state;
-	private String zip;
-	private String country;
-	
-	
-	
-	
-	
-	
-	
-	
+    /*
+      이름 	널? 		유형
+      ------- -------- ------------
+      ADDR_ID 	NOT NULL NUMBER(11)
+      STREET 	NOT NULL VARCHAR2(50)
+      CITY 	NOT NULL VARCHAR2(50)
+      STATE 	NOT NULL VARCHAR2(50)
+      ZIP				VARCHAR2(10)
+      COUNTRY NOT NULL VARCHAR2(50)
+     */
+    @Id
+    @SequenceGenerator(name = "ADDRESS_ADDR_ID_SEQ", sequenceName = "ADDRESS_ADDR_ID_SEQ", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ADDRESS_ADDR_ID_SEQ")
+    private Long addrId;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+
+    @OneToMany(mappedBy = "address", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<Student> students = new ArrayList<>();
+
+    @OneToMany(mappedBy = "address", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<Tutor> tutors = new ArrayList<>();
+
 }
