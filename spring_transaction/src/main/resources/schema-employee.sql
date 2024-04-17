38d2577b222f62fba15fbf21cdd70932132c2a69
Jin <yj999900@gmail.com>
2024-04-17 17:47:04 +0900

    .

---
+++ spring_transaction/src/main/resources/schema-employee.sql
@@ -0,0 +1,10 @@
+drop table employee CASCADE CONSTRAINTS;
+drop sequence employee_id_seq;
+CREATE SEQUENCE employee_id_seq start with 1;
+CREATE TABLE employee (
+  id  NUMBER(4)  PRIMARY KEY,
+  name VARCHAR(30),
+  email  VARCHAR(30),
+  phone VARCHAR(30),
+  salary NUMBER(8) default 0
+);
