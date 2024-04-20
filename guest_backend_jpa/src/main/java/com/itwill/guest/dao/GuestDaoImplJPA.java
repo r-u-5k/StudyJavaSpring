65df9ffbd29e21283d964d9e99157dc4cec039a1
Jin <yj999900@gmail.com>
2024-04-20 16:15:57 +0900

    .

--- guest_backend_jpa/src/main/java/com/itwill/guest/dao/GuestDaoImplJPA.java
+++ guest_backend_jpa/src/main/java/com/itwill/guest/dao/GuestDaoImplJPA.java
@@ -1,10 +1,12 @@
 package com.itwill.guest.dao;
 
 import com.itwill.guest.dto.GuestDto;
+import com.itwill.guest.entity.Guest;
 import com.itwill.guest.repository.GuestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 @Repository("guestDaoImplJPA")
 public class GuestDaoImplJPA implements GuestDao {
@@ -12,43 +14,63 @@ public class GuestDaoImplJPA implements GuestDao {
     GuestRepository guestRepository;
 
     @Override
-    public GuestDto insert(GuestDto guestDto) throws Exception {
-        return guestRepository.save(guestDto);
+    public int insert(GuestDto guestDto) throws Exception {
+        Guest guestEntity = Guest.toEntity(guestDto);
+        guestRepository.save(GuestDto.toDto(guestEntity));
+        return 0;
     }
 
     @Override
-    public GuestDto update(GuestDto guestDto) throws Exception {
-        return guestRepository.save(guestDto);
+    public int update(GuestDto guestDto) throws Exception {
+        Guest guestEntity = Guest.toEntity(guestDto);
+        guestRepository.save(guestDto);
+        return 0;
     }
 
     @Override
-    public int delete(int guestNo) throws Exception {
-        GuestDto guestDto = guestRepository.findByGuestNo(guestNo);
-        return guestRepository.delete(guestDto);
+    public void delete(int guestNo) throws Exception {
+        Guest guestEntity = guestRepository.findByGuestNo(guestNo);
+        guestRepository.delete();
     }
 
     @Override
     public GuestDto findByGuestNo(int guestNo) throws Exception {
-        return guestRepository.findByGuestNo(guestNo);
+        Guest guestEntity = guestRepository.findByGuestNo(guestNo);
+        return GuestDto.toDto(guestEntity);
     }
 
     @Override
     public List<GuestDto> findByAll() throws Exception {
-        return guestRepository.findAll();
+        List<Guest> guestEntityList = guestRepository.findAll();
+        List<GuestDto> guestDtoList = new ArrayList<>();
+        for (Guest guestEntity : guestEntityList) {
+            guestDtoList.add(GuestDto.toDto(guestEntity));
+        }
+        return guestDtoList;
     }
 
     @Override
     public List<GuestDto> findByGuestName(String guestName) throws Exception {
-        return guestRepository.findByGuestName(guestName);
+        List<Guest> guestEntityList = guestRepository.findByGuestName(guestName);
+        List<GuestDto> guestDtoList = new ArrayList<>();
+        for (Guest guestEntity : guestEntityList) {
+            guestDtoList.add(GuestDto.toDto(guestEntity));
+        }
+        return guestDtoList;
     }
 
     @Override
     public List<GuestDto> findByGuestTitle(String guestTitle) throws Exception {
-        return guestRepository.findByGuestTitle(guestTitle);
+        List<Guest> guestEntityList = guestRepository.findByGuestTitle(guestTitle);
+        List<GuestDto> guestDtoList = new ArrayList<>();
+        for (Guest guestEntity : guestEntityList) {
+            guestDtoList.add(GuestDto.toDto(guestEntity));
+        }
+        return guestDtoList;
     }
 
     @Override
     public List<GuestDto> findByGuestContent(String guestContent) throws Exception {
-        return guestRepository.findByGuestContent(guestContent);
+        List<Guest> guestEntityList = guestRepository.findByGuestContent(guestContent);
     }
 }
