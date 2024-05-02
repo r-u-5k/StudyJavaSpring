98b68ce35d96a9e01e5c9ddd6067adef3f6d56e2
Jin <yj999900@gmail.com>
2024-05-02 17:39:10 +0900

    .

--- spring_security_oauth_authentication/src/main/java/com/springboot/security/oauth/auth/userinfo/Oauth2UserInfo.java
+++ spring_security_oauth_authentication/src/main/java/com/springboot/security/oauth/auth/userinfo/Oauth2UserInfo.java
@@ -4,8 +4,12 @@ import java.util.Map;
 
 public interface Oauth2UserInfo {
     Map<String, Object> getAttributes();
+
     String getProviderId();
+
     String getProvider();
+
     String getEmail();
+
     String getName();
 }
